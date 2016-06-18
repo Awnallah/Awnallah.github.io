1ef9a026b9a2a6e1b37135a9f2a897096663bbbc
Hamzah <6hamzah6@gmail.com>
2016-06-18 14:14:05 -0700

    remove unused prop

--- src/js/app.js
+++ src/js/app.js
@@ -8,34 +8,27 @@ angular.module('portfolio', [])
                           name:'Running Map',
                           image: 'src/images/map.png',
                           link:'http://awnallah.github.io/MapProject/',
-                          githubLink:'https://github.com/Awnallah/MapProject',
-                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comment: ''
+                          githubLink:'https://github.com/Awnallah/MapProject'
+
                         },
                         {
                           name:'Arcade Game',
                           image: 'src/images/game1.png',
                           link:'http://awnallah.github.io/frontend-nanodegree-arcade-game/',
-                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-arcade-game',
-                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comment: ''
+                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-arcade-game'
                         },
                         {
                           name:'Website Optimization',
                           image: 'src/images/optimize.png',
                           link:'http://awnallah.github.io/frontend-nanodegree-mobile-portfolio/',
-                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-mobile-portfolio',
-                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comment: ''
+                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-mobile-portfolio'
                         },
                         {
                           name:'Running Map',
                           image: 'src/images/map.png',
                           category: 'mains',
                           link:'http://awnallah.github.io/MapProject/',
-                          githubLink:'https://github.com/Awnallah/MapProject',
-                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comment: ''
+                          githubLink:'https://github.com/Awnallah/MapProject'
                         }
 
                         ];
@@ -43,4 +36,4 @@ angular.module('portfolio', [])
 
 
 
-        	}])
+        	}]);
