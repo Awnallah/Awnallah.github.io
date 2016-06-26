1180380e67ef52bf4f79128ce572dcb7a7dc0864
Hamzah <6hamzah6@gmail.com>
2016-06-26 16:34:12 -0700

    add skills ul

--- src/js/app.js
+++ src/js/app.js
@@ -8,26 +8,30 @@ angular.module('portfolio', [])
                           name:'Running Map',
                           image: 'src/images/map.png',
                           link:'http://awnallah.github.io/MapProject/',
-                          githubLink:'https://github.com/Awnallah/MapProject'
+                          githubLink:'https://github.com/Awnallah/MapProject',
+                          skills:['OOP-JS', 'Knouckout-JS', 'AJAX', 'Bootstrap']
 
                         },
                         {
                           name:'Arcade Game',
                           image: 'src/images/game1.png',
                           link:'http://awnallah.github.io/frontend-nanodegree-arcade-game/',
-                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-arcade-game'
+                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-arcade-game',
+                          skills:['OOP-JS', 'Bootstrap', 'HTML5-Canvas']
                         },
                         {
                           name:'Website Optimization',
                           image: 'src/images/optimize.png',
                           link:'http://awnallah.github.io/frontend-nanodegree-mobile-portfolio/',
-                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-mobile-portfolio'
+                          githubLink:'https://github.com/Awnallah/frontend-nanodegree-mobile-portfolio',
+                          skills:['Develpor Tools', 'critical rendering path', '60FPS rendering']
                         },
                         {
                           name:'Online Resume',
                           image: 'src/images/resume.png',
                           link:'http://awnallah.github.io/resume/',
-                          githubLink:'https://github.com/Awnallah/resume'
+                          githubLink:'https://github.com/Awnallah/resume',
+                          skills:['jQuery', 'CSS3', 'Bootstrap']
                         }
 
                         ];
